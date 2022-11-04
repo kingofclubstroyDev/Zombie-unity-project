cc455ec7a37bd00509fa4f3dace7e47ba0f41841
kingofclubstroy <100493790+kingofclubstroyDev@users.noreply.github.com>
2022-11-04 11:39:27 -0400

    issue resolved and added readme
    fixed issue where when a new target was chosen, it would always choose an non zombie, leading to friendly fire... added readme

--- Assets/Scripts/AI Scripts/Actions/AttackAction.cs
+++ Assets/Scripts/AI Scripts/Actions/AttackAction.cs
@@ -9,8 +9,21 @@ public class AttackAction : Action
     {   
         if(controller.target == null) return;
 
-        if(Vector3.Distance(controller.transform.position, controller.target.transform.position) <= controller.AIVariables.attackRange) {
+        MonoBehaviour.print(controller.transform.position);
 
+        MonoBehaviour.print("-----");
+        MonoBehaviour.print(controller.target.transform.position);
+
+        float distance = Vector3.Distance(controller.transform.position, controller.target.transform.position);
+        
+
+        MonoBehaviour.print("distance = " + distance);
+
+        MonoBehaviour.print("attack range = " + controller.AIVariables.attackRange);
+
+        if(distance <= controller.AIVariables.attackRange) {
+
+            MonoBehaviour.print("attacking");
             //todo add some actuall attacking
             controller.attack();
 
---
+++ README.md
@@ -0,0 +1,9 @@
+# Zombie unity project
+
+Building out the start of a rts/autobattler roguelike game, where the user controls/manages an army of zombies, fighting against other human armies, where you can infect and expand your army and specialize it overtime with mutations and tactics.
+
+The main idea is to build a game where units have a plug and play finite state ai, where tactics and behavior can be simply added, giving the user some control like commands and more optimal behaviour, but mostly the fight plays out as an autobattler. Choices for upgrades and behavior/tactics will be given, in a typical rougelike style.
+
+# Note
+
+This is a work in progress
