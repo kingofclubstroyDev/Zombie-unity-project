eb481450a20eeb0709a81204bef4e59720bcdf34
kingofclubstroy <100493790+kingofclubstroyDev@users.noreply.github.com>
2022-11-24 16:37:07 -0500

    improved kiting ai tactic

--- Assets/Scripts/AI Scripts/Actions/KiteTarget.cs
+++ Assets/Scripts/AI Scripts/Actions/KiteTarget.cs
@@ -9,20 +9,26 @@ public class KiteTarget : Action
 
         if(controller.target == null) return;
         
-        if(Vector3.Distance(controller.transform.position, controller.target.transform.position) < controller.AIVariables.attackRange) {
+        //kite if the target is closer than 75% the max attack range
+        if(Vector3.Distance(controller.transform.position, controller.target.transform.position) < controller.AIVariables.attackRange * 0.75) {
 
             float timeSinceLastAttack = controller.getTimeToAttack();
 
             float fifthAttackSpeed = controller.AIVariables.attackSpeed / 5;
 
+            // don't move if the target is about to attack, or has jut recently attacked, to mimic attack prep, and after shot
             if(timeSinceLastAttack < fifthAttackSpeed || timeSinceLastAttack > (fifthAttackSpeed * 4)) {
                 return;
             }
 
             Vector3 direction = (controller.target.transform.position - controller.transform.position).normalized;
 
-            controller.rigidBody.AddForce(((Quaternion.Euler(0,180,0) * direction) * controller.AIVariables.moveSpeed / 20), ForceMode.Acceleration);
+            Vector3 newDirection = direction * controller.AIVariables.moveSpeed * Time.deltaTime;
 
+            // move at half speed away when kiting
+            Vector3 newPosition = controller.transform.position - (direction * ((controller.AIVariables.moveSpeed / 2) * Time.deltaTime));
+
+            controller.transform.position = newPosition;
             
             return;
 
